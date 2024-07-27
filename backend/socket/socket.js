c88edbeafb0473b7c3b533fc1e14eb180f58e78d
Jatinkr07 <jatinkumar07911@gmail.com>
2024-07-27 16:46:41 +0530

    first commit

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://fizz-chat-v11.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
